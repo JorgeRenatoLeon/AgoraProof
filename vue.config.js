2c1a213d81c248ac7e7435e87cd764b025350855
Jorge Leon <jrleon@pucp.edu.pe>
2021-05-13 18:43:17 -0500

    config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'    ? '/basic-video-call/'    : '/'
+  publicPath: process.env.NODE_ENV === 'production'    ? '/AgoraProof/'    : '/'
 }
